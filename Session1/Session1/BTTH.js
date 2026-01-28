8a22a5f93163c1231ba488ee912cc95b657d2860
hwp07 <hongphongpham2108@gmail.com>
2026-01-28 17:09:46 +0700

    Add files via upload

---
+++ Session1/Session1/BTTH.js
@@ -0,0 +1,18 @@
+let code_book = prompt("Nhập mã sách: ").trim().toUpperCase();
+let book_id = author + "-" + book + "-";
+console.log("Mã sách: " + book_id);
+
+let book = prompt("Nhập tên sách: ").toUpperCase;
+console.log("Tên sách: " + book);
+
+let author = prompt("Nhập tên tác giả: ");
+console.log("Tên tác giả: " + author);
+
+let year = prompt("Nhập năm xuất bản: ");
+console.log("Năm xuất bản: " + year);
+
+let price = prompt("Nhập giá trị sách: ");
+console.log("Giá tiền của 1 cuốn: " + price);
+
+let suggest = prompt("Nhập ngăn kệ gợi ý: ");
+console.log("Ngăn kệ gợi ý: " + suggest);
---
+++ Session1/Session1/LS5.js
@@ -0,0 +1,7 @@
+let nameBook = prompt("Nhập tên sách: ");
+console.log("Sách: " + nameBook);
+
+let year = +prompt("Năm xuất bản: ");
+console.log("Năm xuất bản: " + year);
+
+console.log(`Tuổi sách: ${2026 - year}`);
