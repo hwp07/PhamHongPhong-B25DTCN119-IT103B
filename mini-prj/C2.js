36558102d68be16d51482241f5281bf2b0cd9f49
hwp07 <hongphongpham2108@gmail.com>
2026-02-02 07:24:55 +0700

    Add file via upload

---
+++ mini-prj/C2.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+    count++;
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+console.log(`Tổng của tất cả các số đã in ra chữ "Fizz": ${count}`);
