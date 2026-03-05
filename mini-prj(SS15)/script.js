da7f4980127d628001300b544554d3837bfcdbc6
hwp07 <hongphongpham2108@gmail.com>
2026-03-05 14:43:26 +0700

    Update file

--- mini-prj(SS15)/script.js
+++ mini-prj(SS15)/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const form = document.getElementById("form");
 
 form.addEventListener("submit", function (e) {
@@ -16,22 +15,3 @@ form.addEventListener("submit", function (e) {
   };
   console.log(newUser);
 });
-=======
-const form = document.getElementById("form");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const emailInput = document.getElementById("email");
-  const passwordInput = document.getElementById("password");
-
-  const name = emailInput.value;
-
-  const password = passwordInput.value;
-  const newUser = {
-    name: name,
-    password: password,
-  };
-  console.log(newUser);
-});
->>>>>>> ea34099f53489974fb8c1e6af8060b8244db0e51
